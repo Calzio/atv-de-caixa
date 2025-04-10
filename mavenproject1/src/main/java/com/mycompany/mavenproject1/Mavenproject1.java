412b2828f37ea10936c089a67e46f22fc9133ca9
Calzio <162699126+Calzio@users.noreply.github.com>
2025-04-10 11:01:02 -0300

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ * Swing
+ * @author aluno.den
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
